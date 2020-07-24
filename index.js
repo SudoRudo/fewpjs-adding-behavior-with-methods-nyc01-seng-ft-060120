88e27f4d47254b3192f1b717967abf322ed67fb4
Rudy Guerrero <r.guerreronyc@gmail.com>
2020-07-24 00:58:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
 // Your code here
 
 class Cat {
-  constructor(name, sex)
+  constructor(name, sex){
   this.name = name
   this.sex = sex 
+  }
+  
+  speak(){
+    `${this.name} says meow!`
+  }
 }
 
 class Dog {
-  constructor(name, sex)
+  constructor(name, sex){
   this.name = name
   this.sex = sex 
+  }
+  
+  speak(){
+    `${this.name} says woof!`
+  }
 }
 
 class Bird {
-  constructor(name, sex)
+  constructor(name, sex){
   this.name = name
-  this.sex = sex 
+  this.sex = sex
+  }
+  
+  speak(){
+    if (this.sex === "male"){
+      `It's me! ${this.name}, parrot!`
+    } else {
+    `${this.name} says squawk!`
+    }
+  }
 }
+
+
+
+
+
